583d67a72aa180e2eba49fc96d50b48898e41d70
Conner, William (wc1091) <wc1091@att.com>
2021-05-21 18:42:23 -0400

    implemented majority of buy logic

--- invest.py
+++ invest.py
@@ -50,25 +50,36 @@ def updateCredentials(args):
             break
 
     writeJSON(config['credentialsPath'], credentials)
-
     print('credentials updated')
 
-def login():
+def login(args=''):
     global isLoggedIn
     if (not isLoggedIn):
         print('Logging into robinhood')
         if (not os.path.exists(config['credentialsPath'])):
             raise Exception("No credentials exist. Please create them")
         credentials=readJSON(config['credentialsPath'])
-        loginx = rh.login(credentials['username'],'password')
+        isLoggedIn = rh.login(credentials['username'],credentials['password'])
 
 def showInvestments(args):
     print("showInvestments")
     for item in readJSON(config['investmentsPath']):
         print(item['symbol'])
 
+def readTrueFalse(prompt):
+    while True:
+        result = input(prompt)
+        if (result.lower() in ['y','yes','true']):
+            return True
+        if (result.lower() in ['n','no','false']):
+            return False
+
+# I can't decide. Do I want to mannage investments via watchlists on rh or in a local file?
+# If via watch list, you'd have to add a new stock to the list prior to investing
+#  you'd still have to keep a local file if you wanted to weight some securities over others
+# For now I think it's better to manage everything in a local file
 def buyInvestments(args):
-    print("buyInvestments")
+    print(f'buyInvestments')
     login()
 
     investments=readJSON(config['investmentsPath'])
@@ -76,20 +87,28 @@ def buyInvestments(args):
     for item in investments:
         symbols.append(item['symbol'])
 
-    #item['pending_average_buy_price']/
-    spread=math.floor(args.amount/len(investments))
-    #print(spread)
+    current_prices = rh.get_latest_price(symbols)
+    spread=math.floor(args.amount/len(investments)) #equally divided for now
 
-    #current_prices = rh.get_latest_price(symbols)
-    #print(current_prices)
-    for item in investments:
-        symbol=item['symbol']
+    plan=[]
+    for security in investments:
+        symbol=security['symbol']
+        price=float(current_prices.pop(0))
+        quantity=price/spread
+        plan.append({'symbol':symbol, 'quantity':quantity})
 
-        #I don't like all of the rest calls. maybe use rh.get_latest_price(symbols)
-        quanity=float(rh.get_latest_price(item['symbol']).pop())/spread
-        print(f"buy symbol: {symbol:>4s} quanity: {quanity:13.8f}")
-        #rh.order_buy_market('AAPL',10)
-        #print('buy '+item['symbol']+' ')
+    for security in plan:
+        print(f"buy symbol: {security['symbol']:>4s} quantity: {security['quantity']:13.8f}")
+        
+    if (args.dryrun):
+        print('Dryrun. No action taken')
+        return
+        
+    if (readTrueFalse('execute trade (y/n)?')):
+        for security in plan:
+            print(f"executed {security['symbol']:>4s} quantity: {security['quantity']:13.8f}")
+    else:
+        print('No action taken')
 
 def showPositions(args):
     print("showPositions")
@@ -99,31 +118,31 @@ def showPositions(args):
     for item in positions_data:
         #print(item)
         print(f"symbol: {rh.get_symbol_by_url(item['instrument']):>4s} quanity: {float(item['quantity']):13.8f} cost: {float(item['quantity'])*float(item['average_buy_price']):13.8f}")
-        
-# investments is a json file containing stock or crypto objects
 
 
-#stocks_dir='stocks'
+# Notes
+# Credentials needs to be created first
+# inventments.json holds the list of all securities and crypto being traded
+#
+# 
+# TODO
+# The dollar amoount is equally divided across all securities. Need to implement weighting
+# Implement crypto    
 
-#stocks_path='fool.pk1'
-#stock_list = [Stock('TTC', 'stock', 100),Stock('ABNB', 'stock', 100),Stock('IDXX', 'stock', 100),Stock('MTN', 'stock', 100),Stock('OKTA', 'stock', 100),Stock('UNP', 'stock', 100),Stock('TTD', 'stock', 100),Stock('TEAM', 'stock', 100),Stock('SHOP', 'stock', 100),Stock('FVRR', 'stock', 100),Stock('CRWD', 'stock', 100)]
-#stocks = loadStocks(stocks_path)
-#for item in stocks:
-#    print(item.ticker)
-
-#login = rh.login()
-#positions_data = rh.get_watchlist_by_name(name='Fool')
-#print(positions_data)
-#for item in positions_data['results']:
-#    print(item['symbol'])
-
-#invest buy 1000 --dryrun --ticker
-#invest add AAPL
+# command scratch pad
+#invest credentials
+#invest show (not liking this name)
+#invest positions
+#invest buy 1000 --dryrun --symbol (list)
+#invest add AAPL (need weighting and type)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='rhinvest')
     subparsers = parser.add_subparsers()
     
+    parser_login = subparsers.add_parser('login', help='login to robinhood')
+    parser_login.set_defaults(func=login)
+
     parser_credentials = subparsers.add_parser('credentials', help='create or update stored credentials')
     parser_credentials.set_defaults(func=updateCredentials)
 
@@ -137,7 +156,6 @@ if __name__ == '__main__':
     parser_buy.set_defaults(func=buyInvestments)
     parser_buy.add_argument('amount', type=int, help='The amount of money in dollars you want to invest')
     parser_buy.add_argument('--dryrun', action='store_true', help='do not perform any actions')
-
 
     args = parser.parse_args()
     args.func(args)
